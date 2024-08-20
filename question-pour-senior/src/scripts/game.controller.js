914dd2c6b04f49f1ac494640e8a4be1b321bc1a9
Mohamed Al Glawi <mohamed.alglawi@gmail.com>
2024-08-20 23:00:24 +0200

    add controller pages

--- question-pour-senior/src/scripts/game.controller.js
+++ question-pour-senior/src/scripts/game.controller.js
@@ -1,6 +1,57 @@
 class ControllerGame {
     constructor (legend) {
         this.legend = legend;
+        this.stages = ["home", "identification", "plays", "game"]
+        this.currentStage = ""
+
+        this.hideAllStages();
+        this.initEvents();
+        this.initializeGame();
+
+        
     }
+
+    hideAllStages() {
+        for(let stage of this.stages) {
+            this.hideSatge(stage);
+        }
+    }
+
+    hideSatge(stageName) {
+        $('#controller #controller-'+stageName).hide();
+    }
+
+    showStage(stageName) {
+        this.hideAllStages();
+        $('#controller #controller-'+stageName).show();
+
+    }
+
+    initializeGame() {
+        //set default state
+        // steps : home, identify, thems, play
+        this.currentStage = this.stages[0];
+        this.legend.setState({
+            stage:  this.stages[0],
+            players:   [],
+            question:  null,
+            responses: [],
+            buzzing: null,
+        });
+    }
+
+    initEvents() {
+        // State change
+        this.legend.onStateChange((from, state) => {
+            if (this.currentStage != state["stage"]) {
+                this.currentStage = state["stage"];
+                this.hideAllStages();
+                this.showStage(this.currentStage)
+            }
+
+        });
+    }
+
+
    
 }
--- question-pour-senior/src/scripts/game.monitor.js
+++ question-pour-senior/src/scripts/game.monitor.js
@@ -1,7 +1,43 @@
 class MonitorGame {
     constructor(legend) {
         this.legend = legend;
+        this.stages = ["home", "identification", "plays", "game"]
+        this.currentStage = ""
+        this.hideAllStages();
+        this.initEvents();
 
+
+    }
+    hideAllStages() {
+        for(let stage of this.stages) {
+            this.hideSatge(stage);
+        }
+    }
+
+    hideSatge(stageName) {
+        $('#monitor #monitor-'+stageName).hide();
+    }
+
+    showStage(stageName) {
+        this.hideAllStages();
+        $('#monitor #monitor-'+stageName).show();
+
+    }
+
+    initEvents() {
+        // State change
+        this.legend.onStateChange((from, state) => {
+            if (this.currentStage != state["stage"]) {
+                this.currentStage = state["stage"];
+                this.hideAllStages();
+                this.showStage(this.currentStage)
+            }
+            
+            
+
+
+
+        });
     }
     
 }
